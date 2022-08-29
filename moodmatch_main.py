3bfc267d901fb198130050547cc942542e80099a
alexanderrizzi <108607624+alexanderrizzi@users.noreply.github.com>
2022-08-29 11:53:52 +0530

    Update moodmatch_main.py

--- moodmatch_main.py
+++ moodmatch_main.py
@@ -11,10 +11,7 @@ st.header('Would you rather like a song or a quote?')
 selected_choice = st.selectbox('Pick', ['Song','Quote'])
 
 if(selected_choice=='Quote'):
-  data_load_state=st.text("Loading the perfect Quote for you . . . ")
-  with st.spinner(text='In progress'):
+  with st.spinner(text='Loading the perfect Quote for you . . .'):
    time.sleep(5)
-   st.success('Done')
-
   quote =  ' "The best revenge is not to be like that" - Marcus Aurelius '
   st.subheader(quote)
