6a84a3bf444d580898f7c5de52ac2232b5a850d9
alexanderrizzi <108607624+alexanderrizzi@users.noreply.github.com>
2022-08-29 11:48:34 +0530

    Update moodmatch_main.py

--- moodmatch_main.py
+++ moodmatch_main.py
@@ -11,5 +11,8 @@ selected_choice = st.selectbox('Pick', ['Song','Quote'])
 
 if(selected_choice=='Quote'):
   data_load_state=st.text("Loading the perfect Quote for you . . . ")
+  with st.spinner(text='In progress'):
+   time.sleep(5)
+   st.success('Done')
   quote =  ' "The best revenge is not to be like that" - Marcus Aurelius '
   st.subheader(quote)
