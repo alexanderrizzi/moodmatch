6e69c353c81d0657d05e8787ab3074662c2b75eb
alexanderrizzi <108607624+alexanderrizzi@users.noreply.github.com>
2022-08-29 11:21:46 +0530

    Update moodmatch_main.py

--- moodmatch_main.py
+++ moodmatch_main.py
@@ -2,7 +2,9 @@ import streamlit as st
 
 st.title("Whats your Mood . . ?")
 
-st.selectbox('Choose', ['Happy', 'Sad'])
 
-input =  st.selectbox()
+emotions = ['Happy', 'Sad']
+selected_emotion = st.selectbox('Choose', emotions)
+
+input =  selected_emotion
 st.text(input)
