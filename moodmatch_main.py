19f993c012ff30490ebff715d1e02332bd3dfd23
alexanderrizzi <108607624+alexanderrizzi@users.noreply.github.com>
2022-09-01 16:22:13 +0530

    Update moodmatch_main.py

--- moodmatch_main.py
+++ moodmatch_main.py
@@ -11,28 +11,42 @@ selected_mood = st.selectbox('Choose', moods)
 st.header('Would you rather like a song or a quote?')
 selected_choice = st.selectbox('Pick', ['Song','Quote'])
 
-if(selected_choice=='Quote'):
-  with st.spinner(text='Loading the perfect Quote for you . . .'):
-   time.sleep(4)
-
 quotes=pd.read_csv('quotesDB.csv')
 
 st.text (quotes['Rating'][0][3])
   
 dict1 = {
   "Happy": 0,
-  "Sad": 1,
-  "Disgust": 2 ,
-  "Afraid": 3,
-  "Angry": 4,
-  "Grateful": 5, 
-  "Nostalgic": 6,
-  "Empathic": 7,
-  "Romantic": 8,
-  "Inspired": 9,
-  "Broken": 10,
-  "I dont know": 11
+  "Sad": 2,
+  "Disgust": 4 ,
+  "Afraid": 6,
+  "Angry": 8,
+  "Grateful": 10, 
+  "Nostalgic": 12,
+  "Empathic": 14,
+  "Romantic": 16,
+  "Inspired": 18,
+  "Broken": 20,
+  "I dont know": 22
 }
-  
+
 if(dict1[selected_mood]<11):
-  st.text("ss")
+  max=0
+  arr=[0]*6
+  for i in range(0,5):
+    # for j in np.arange(0,22,2):
+    if(Rating[i][dict1[selected_mood]] > max): 
+        max=Rating[i][dict1[selected_mood]]
+        arr[k]=i
+        k+=1
+    if(len(arr)==1): quote= quotes["List"][arr[0]]
+    else: 
+        rng = np.random.default_rng() # doing this is recommended by numpy
+        non_zero = arr[arr!=0]
+        rand=rng.choice(non_zero, 1, replace=False)
+        quote= quotes["List"][arr[rand]]
+        
+if(selected_choice=='Quote'):
+  with st.spinner(text='Loading the perfect Quote for you . . .'):
+   time.sleep(4)
+st.subheader(quote)
