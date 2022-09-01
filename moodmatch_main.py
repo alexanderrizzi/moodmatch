9ec348a46ea57c2e5293396b3d6023107141afc3
alexanderrizzi <108607624+alexanderrizzi@users.noreply.github.com>
2022-09-01 15:51:36 +0530

    Update moodmatch_main.py

--- moodmatch_main.py
+++ moodmatch_main.py
@@ -9,7 +9,7 @@ selected_emotion = st.selectbox('Choose', moods)
 
 
 st.header('Would you rather like a song or a quote?')
-selected_choice = st.selectbox('Pick', ['Song','Quote'])
+selected_mood = st.selectbox('Pick', ['Song','Quote'])
 
 if(selected_choice=='Quote'):
   with st.spinner(text='Loading the perfect Quote for you . . .'):
@@ -20,3 +20,20 @@ quotes=pd.read_csv('quotesDB.csv')
 for i in range(0,11):
   st.text (quotes['Rating'][1][i])
 
+  dict1 = {
+  "Happy": 0,
+  "Sad": 1,
+  "Disgust": 2
+   "Afraid": 3,
+  "Angry": 4,
+  "Grateful": 5
+   "Nostalgic": 6,
+  "Empathic": 7,
+  "Romantic": 8
+   "Inspired": 9,
+  "Broken": 10,
+  "I dont know": 11
+}
+  
+if (thisdict(selected_mood)<12):
+  st.text("ss")
