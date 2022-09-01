a0a0cf7613e7f7272f4d0a259171bf15caa9054f
alexanderrizzi <108607624+alexanderrizzi@users.noreply.github.com>
2022-09-01 15:58:03 +0530

    Update moodmatch_main.py

--- moodmatch_main.py
+++ moodmatch_main.py
@@ -5,13 +5,13 @@ import pandas as pd
 st.title("How are You feeling Today?")
 
 moods = ['Happy','Sad','Disgust','Afraid','Angry','Grateful','Nostalgic','Empathic','Romantic','Inspired','Broken','I dont know']
-selected_emotion = st.selectbox('Choose', moods)
+selected_mood = st.selectbox('Choose', moods)
 
 
 st.header('Would you rather like a song or a quote?')
-selected_mood = st.selectbox('Pick', ['Song','Quote'])
+selected_choice = st.selectbox('Pick', ['Song','Quote'])
 
-if(selected_mood=='Quote'):
+if(selected_choice=='Quote'):
   with st.spinner(text='Loading the perfect Quote for you . . .'):
    time.sleep(4)
 
