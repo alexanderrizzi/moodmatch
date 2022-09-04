ee4c5f3f6d47a40e67e04570c0d8d9466050fd20
alexanderrizzi <108607624+alexanderrizzi@users.noreply.github.com>
2022-09-04 15:22:39 +0530

    Update moodmatch_main.py

--- moodmatch_main.py
+++ moodmatch_main.py
@@ -53,7 +53,7 @@ if(dict1[selected_mood]<22):
     st.text(pos_arr)
     if(len(pos_arr)==1): quote= quotes["List"][pos_arr[0]]    
     else: 
-        rnd=random.randint(0,len(pos_arr))
+        rnd=random.choice(pos_arr)
         quote= quotes["List"][rnd]
         
 if(selected_choice=='Quote'):
